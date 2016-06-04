cf4ac2d9a0afe283e106ada468926339bce92756
StanPast <pastfama@gmail.com>
2016-06-03 18:56:52 -0700

    get_all

--- stockreport.py
+++ stockreport.py
@@ -32,3 +32,31 @@ def post_stock_price(symbol, apikey, devicename):
     postime = datetime.now()
     stock_price = ystockquote.get_price(symbol).encode('utf-8')
     stream.add_value(stock_price, postime)
+
+
+
+def post_stock_info(symbol, apikey, devicename, ):
+    '''
+    Retrieve the stock price for the given ticker symbol ("T" for AT&T) and
+    post it in the correct M2X data stream.
+    '''
+    client = M2XClient(key=apikey)
+
+    # Find the correct device if it exists, if not create it.
+    try:
+        device = [d for d in client.devices(q=devicename) if d.name == devicename][0]
+    except IndexError:
+        device = client.create_device(name=devicename,
+                                      description="Stockreport Device",
+                                      visibility="private")
+
+    # Get the stream if it exists, if not create the stream.
+    try:
+        stream = device.stream(symbol)
+    except HTTPError:
+        stream = device.create_stream(symbol)
+        device.update_stream(symbol, unit={'label': 'Dollars', 'symbol': '$'})
+
+    postime = datetime.now()
+    stock_price = ystockquote.get_all(symbol).encode('utf-8')
+    stream.add_value(stock_price, postime)
